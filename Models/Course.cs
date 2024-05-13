ac50901a1ab62ff0ff83aa6b1b63ea1bd59e2611
LazarevicV <vukadinlazarevic@gmail.com>
2024-05-13 15:47:53 +0200

    dodati modeli

---
+++ Models/Course.cs
@@ -0,0 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace asp_net_react_fullstack_app.Server.Models;
+
+public class Course
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Category { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string Link { get; set; }
+    
+    public string Schools { get; set; }
+}
