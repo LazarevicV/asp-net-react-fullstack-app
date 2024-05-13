8b06f1126687ad157ed46324e59c448afda06242
LazarevicV <vukadinlazarevic@gmail.com>
2024-05-13 15:48:53 +0200

    user model added

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace asp_net_react_fullstack_app.Server.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    
+    public string Username { get; set; }
+    
+    public string Password { get; set; }
+    
+    public string Role { get; set; }
+}
