7dd494c3d6f66694fa5f3e54dac1714569f90e7c
LazarevicV <vukadinlazarevic@gmail.com>
2024-05-13 16:36:34 +0200

    added controller for courses

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,45 @@
+using asp_net_react_fullstack_app.Server.Models;
+using asp_net_react_fullstack_app.Server.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_react_fullstack_app.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController(CoursesService coursesService) : ControllerBase
+    {
+        // GET: api/Courses
+        [HttpGet]
+        public async Task<ActionResult<List<Course>>> GetCourses()
+        {
+            var courses = await coursesService.GetAllCoursesAsync();
+            return Ok(courses);
+        }
+
+        // GET api/<CoursesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CoursesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CoursesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CoursesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
