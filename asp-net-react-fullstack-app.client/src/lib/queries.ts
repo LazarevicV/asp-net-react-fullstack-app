8402d162466a913a01583e63532db9f06efe801c
Marko Bozic <markobozic346@gmail.com>
2024-05-28 23:02:04 +0200

    Add queries for update and delete

--- asp-net-react-fullstack-app.client/src/lib/queries.ts
+++ asp-net-react-fullstack-app.client/src/lib/queries.ts
@@ -19,3 +19,51 @@ export const deleteCourse = async (id: string) => {
     },
   });
 };
+
+export const getCoursesCategories = async (): Promise<string[]> => {
+  const res = await api({ endpoint: "api/Courses/Categories" });
+  return res.data;
+};
+
+export const updateCourse = async (course: CourseType & { file: File | null}, ) => {
+
+  const { id, ...rest } = course;
+  return await api({
+    endpoint: `api/Courses/${id}`,
+    config: {
+      method: "PUT",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: {
+        Title: rest.title,
+        Description: rest.description,
+        Link: rest.link,
+        Category: rest.category,
+        School: rest.school,
+        File: rest.file,
+
+      },
+    },
+  });
+}
+
+export const createCourse = async (course: CourseType & { file: File | null }) => {
+  return await api({
+    endpoint: `api/Courses`,
+    config: {
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: {
+        Title: course.title,
+        Description: course.description,
+        Link: course.link,
+        Category: course.category,
+        School: course.school,
+        File: course.file,
+      },
+    },
+  });
+};
